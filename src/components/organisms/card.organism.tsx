34bc912dabbef02217893e2c1498261fd438474f
Alexander <alexvanheteren@gmail.com>
2023-06-05 12:51:49 +0200

    z-index werkt

--- src/components/organisms/card.organism.tsx
+++ src/components/organisms/card.organism.tsx
@@ -26,22 +26,59 @@ export interface CardProps {
     listingDate: string,
 }
 
+// const Card: React.FC<CardProps> = ({ id, ownerId, animalName, animalSpecies, animalBreed, animalSize, animalImageLink, price, isMale, description, address, age, listingDate }) => {
+//     const [isClick, setClick] = React.useState(false);
+//     return (
+//         <div className={styles.card}>
+//             <Image src={animalImageLink} alt={animalName} className={styles.card_image} width={250} height={250}/>
+//             <div className={styles.card_bottom}>
+//                 <div className={styles.card_info}>
+//                     <h3 className={styles.card_title}>{animalName}</h3>
+//                     <p className={styles.card_description}>{animalBreed}</p>
+//                 </div>
+//                 <div className={styles.card_like}>
+//                     <Heart isClick={isClick} onClick={() => setClick(!isClick)}/>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+ 
+
+ 
 const Card: React.FC<CardProps> = ({ id, ownerId, animalName, animalSpecies, animalBreed, animalSize, animalImageLink, price, isMale, description, address, age, listingDate }) => {
-    const [isClick, setClick] = React.useState(false);
-    return (
-        <div className={styles.card}>
-            <Image src={animalImageLink} alt={animalName} className={styles.card_image} width={250} height={250}/>
-            <div className={styles.card_bottom}>
-                <div className={styles.card_info}>
-                    <h3 className={styles.card_title}>{animalName}</h3>
-                    <p className={styles.card_description}>{animalBreed}</p>
-                </div>
-                <div className={styles.card_like}>
-                    <Heart isClick={isClick} onClick={() => setClick(!isClick)}/>
-                </div>
-            </div>
+  const [isClick, setClick] = React.useState(false);
+  const [zIndex, setZIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    setZIndex((prevZIndex) => prevZIndex + 1);
+  }, []);
+
+  const handleClick = () => {
+    setClick(!isClick);
+    setZIndex(0); // Set the lowest value for z-index
+  };
+
+  return (
+    <div className={styles.card} style={{ zIndex }}>
+      <Image src={animalImageLink} alt={animalName} className={styles.card_image} width={250} height={250} />
+      <div className={styles.card_bottom}>
+        <div className={styles.card_info}>
+          <h3 className={styles.card_title}>{animalName}</h3>
+          <p className={styles.card_description}>{animalBreed}</p>
         </div>
-    );
+        <div className={styles.card_like}>
+          <Heart isClick={isClick} onClick={handleClick} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
+
+
+ 
+
+
+ 
