41a355a6740a4f00ede797ae854ddb60ed109085
Tom van Heek <i454147@fhict.nl>
2023-06-06 12:38:28 +0200

    effectively finished favourites/page.tsx
    It still needs a connection to the logged in user. This could be done by locally storing the user. Right now you would have to manually enter the user's desired ID.

--- src/app/favourites/page.tsx
+++ src/app/favourites/page.tsx
@@ -1,17 +1,62 @@
-import Image from "next/image";
+"use client"
 import styles from "./page.module.css";
-import Card from "@/components/organisms/card.organism";
 import { Inter, Montserrat } from "next/font/google";
-import { Footer } from "@/components/organisms/footer.organism";
+import Card, { CardProps } from "@/components/organisms/card.organism";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Favourites() {
+  const [listing, setListings] = useState<CardProps[]>([]);
+  const [userId, setUserId] = useState(""); // State to hold the user ID input
+  const [error, setError] = useState(null); // State to hold the error message
+
+  // Page logic
+  useEffect(() => {
+    if (userId !== "") {
+      fetch(
+        `https://swipperresource.azurewebsites.net/api/GetListingsForUser?id=${userId}`
+      )
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Error fetching data");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setListings(data);
+          setError(null); // Clear any previous errors
+        })
+        .catch((error) => {
+          setError(error.message);
+        });
+    }
+  }, [userId]);
+
+  // Page design
   return (
     <>
       <main className={styles.main}>
-
+        <input
+          type="number"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+          placeholder="Enter User ID"
+        />
+        {error && <p>Error: {error}</p>}
+        <div className={styles.gridContainer}>
+          {listing.map((listing, index) => (
+            <div
+              id={`styles.toppick_${index + 1}`}
+              className={styles.cardWrapper}
+              key={listing?.id}
+            >
+              <Card {...listing} />
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
+  
 }
